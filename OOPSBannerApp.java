3e0421d417aafacb037969b5cb1cb8810a6638fd
DrCatnip <masterpanshul.164@gmail.com>
2026-02-20 21:06:15 +0530

    Coded UC6: Refactored pattern logic into Static Helper Methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,7 @@
 /**
  * OOPS Banner App
- * UC5: Render OOPS as Banner using Inline Array Initialization
- * Goal: Combine array declaration and initialization using String.join()
+ * UC6: Refactor Banner Logic into Functions
+ * Goal: Use static helper methods to abstract character pattern creation.
  * @author Technical Team
  * @version 1.0
  */
@@ -10,62 +10,60 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Inline declaration + initialization
         String[] banner = {
-
-            String.join(" ",
-                    " *****  ",
-                    " *****  ",
-                    " ****** ",
-                    "  ***** "
-            ),
-
-            String.join(" ",
-                    "*     * ",
-                    "*     * ",
-                    "*     * ",
-                    "*     * "
-            ),
-
-            String.join(" ",
-                    "*     * ",
-                    "*     * ",
-                    "*     * ",
-                    "*       "
-            ),
-
-            String.join(" ",
-                    "*     * ",
-                    "*     * ",
-                    " ****** ",
-                    "  ***** "
-            ),
-
-            String.join(" ",
-                    "*     * ",
-                    "*     * ",
-                    "*       ",
-                    "       *"
-            ),
-
-            String.join(" ",
-                    "*     * ",
-                    "*     * ",
-                    "*       ",
-                    "*     * "
-            ),
-
-            String.join(" ",
-                    " *****  ",
-                    " *****  ",
-                    "*       ",
-                    "  ***** "
-            )
+                String.join(" ", getOPattern(0), getOPattern(0), getPPattern(0), getSPattern(0)),
+                String.join(" ", getOPattern(1), getOPattern(1), getPPattern(1), getSPattern(1)),
+                String.join(" ", getOPattern(2), getOPattern(2), getPPattern(2), getSPattern(2)),
+                String.join(" ", getOPattern(3), getOPattern(3), getPPattern(3), getSPattern(3)),
+                String.join(" ", getOPattern(4), getOPattern(4), getPPattern(4), getSPattern(4)),
+                String.join(" ", getOPattern(5), getOPattern(5), getPPattern(5), getSPattern(5)),
+                String.join(" ", getOPattern(6), getOPattern(6), getPPattern(6), getSPattern(6))
         };
 
-        // Enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // O Pattern
+    public static String getOPattern(int row) {
+        String[] pattern = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+        return pattern[row];
+    }
+
+    // P Pattern
+    public static String getPPattern(int row) {
+        String[] pattern = {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        return pattern[row];
+    }
+
+    // S Pattern
+    public static String getSPattern(int row) {
+        String[] pattern = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+        return pattern[row];
     }
 }
